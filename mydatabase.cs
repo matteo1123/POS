7f82669a5d6e91ae86bbbe7254167609ebff8e4e
Matthew Semroska <matthew.semroska@gmail.com>
2021-02-11 08:31:01 -0700

    Add project files.

---
+++ mydatabase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace POS
+{
+    class mydatabase
+    {
+        //Data source 
+        private string strcon = @"Data Source=DESKTOP-31SFJVO;Initial Catalog=mypos;Integrated Security=True";
+        //SQL connection object
+        private SqlConnection Sqlcon;
+
+        public mydatabase()
+        {
+            Sqlcon = new SqlConnection(this.strcon);
+        }
+
+        public DataTable execyteSqlCommand(string sqlCommand)
+        {
+            DataTable dtTable = new DataTable();
+            try
+            {
+                //open sql connection
+                this.Sqlcon.Open();
+                // create sqlAdapter
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCommand, this.Sqlcon);
+                dataAdapter.Fill(dtTable);
+                this.Sqlcon.Close();
+            }
+            catch (Exception error)
+            {
+                Console.Write(error.ToString());
+                dtTable = null;
+            }
+            return dtTable;
+        }
+
+    }
+}
